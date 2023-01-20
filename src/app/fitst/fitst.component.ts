9d3575706193a3c6021bfaba4e71dbc98d0e86b0
Denis <hohol386@ukr.net>
2023-01-20 19:23:09 +0200

    MutationObserver change by concrete object

--- src/app/fitst/fitst.component.ts
+++ src/app/fitst/fitst.component.ts
@@ -12,69 +12,112 @@ export class FitstComponent implements OnInit,OnDestroy{
 
 
 
+
+
   ngOnInit(): void {
 
 
 
-      chrome.tabs.query({active: true}, (tabs)=> {
+      chrome.tabs.query({active: true,currentWindow: true }, (tabs)=> {
         const tab = tabs[0];
         if (tab) {
           chrome.scripting.executeScript(
             {
-              target:{tabId: tab.id!, allFrames: true},
+              target:{tabId: tab.id!},
               func:()=>{
+
+                let result:string =""
+                let div=document.getElementById("qwe")
+                if (div==null) {
+                  div = document.createElement("div");
+                  div.id = "qwe"
+                  div.style.zIndex = '1';
+                  div.style.color = 'red';
+                  div.style.background = 'black';
+                  div.style.visibility = 'visible';
+                  div.style.left = '10%';
+                  div.style.top = '10%';
+                  div.style.position = 'relative';
+
+
+                  (document.getElementById('oframecdnplayer') as Node).appendChild(div)
+                }
+
+
 
                 const config = { attributes: true, childList: true, subtree: true };
                 let observer = new MutationObserver((mutations, observer)=>{
+
+
                 mutations.forEach(mutation=>{
                   mutation.target.childNodes.forEach(innerNode=>{
                           if (innerNode.nodeName ==="SPAN"){
-                            console.log("in_span" + (innerNode as Element).innerHTML)
+
+                           result = (innerNode as Element).innerHTML
+                           let strings:string[] = []
+                            let nodes:Node[] = []
+                            if (div) {
+
+
+
+                                   strings = result.split(' ');
+                                  strings.forEach((innerSplittedSt)=>{
+
+                                    let InnerDivSplittedBySpace = document.createElement('div');
+
+                                    if (div) {
+                                      div.appendChild(InnerDivSplittedBySpace as Node)
+
+                                      InnerDivSplittedBySpace.innerText = innerSplittedSt
+                                    }
+                                  })
+
+
+                            }
+
                           }
                           })
+                })
+
+                  console.log(result)
+
 
 
                 })
-                })
-                observer.observe(document, config);
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-              //   document.addEventListener('DOMNodeInserted', ()=>{
-              //     let pjsdiv = document.getElementById("oframecdnplayer")!;
-              //
-              //       let span = pjsdiv.getElementsByTagName("span")!;
-              //
-              //         let innerText = ""
-              //         if (innerText != span[0].innerHTML)
-              //           innerText = span[0].innerHTML
-              //         console.log(innerText)
-              //     this.text = innerText;
-              //
-              //
-              //
-              //
-              //   });
-                ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
+                let elementById = document.getElementById('oframecdnplayer')!.getElementsByTagName("span")[0] as Node;
 
-
-
-
-
+                if (elementById) {
+                  console.log(elementById + " ArrayLength")
+                  observer.observe(elementById, config);
+                }
               }
+
             },
           )
         } else {
           alert("There are no active tabs")
         }
       })
-
-
   }
 
   ngOnDestroy(): void {
 
   }
 
-
+madeDiv(result:string,node:any){
+    // if (result.indexOf('<br>') !== -1){
+    //   let BrSplit = result.split('<br>');
+    //   BrSplit.forEach((splittedString)=>{
+    //     let divSplittedByBr = document.createElement('div');
+    //     node.appendChild(divSplittedByBr)
+    //     splittedString.split(' ').forEach((innerSplittedSt)=>{
+    //       let InnerDivSplittedBySpace = document.createElement('div');
+    //       InnerDivSplittedBySpace.innerText = innerSplittedSt
+    //     })
+    //   })
+    // }
+  node.innerText = result
+}
 
 }
